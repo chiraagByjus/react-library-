9c97d44e8a6fd367b254045e1552e038fba25105
Chiraag A <chiraag.a01@byjus.com>
2022-10-18 02:52:16 +0530

    feat(new button): new
    BREAKING CHANGE: 2 buttons now

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,10 +4,12 @@ import "./Button.css";
 const Button = (props) => {
   const { variant, children, ...rest } = props;
   return (
-    <button className={`button ${variant} `} {...rest}>
-      {children}
-      Button
-    </button>
+    <>
+      <button className={`button ${variant} `} {...rest}>
+        {children}
+      </button>
+      <button className={`button danger`}>{children}</button>
+    </>
   );
 };
 
